1d3055c91494a67c8139bda61ea8072778c22b1a
juan-skill <1294@holbertonschool.com>
2020-03-13 12:23:49 +0000

    [Add] adding solution for preorder 	new file:   6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_preorder - goes through a binary tree using pre-order traversal
+  *
+  * @tree: is a pointer to the root node of the tree to traverse
+  * @func: is a pointer to a function to call for each node.
+  * The value in the node must be passed as a parameter to this function.
+  * Return: void
+  */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	/* binary_tree_print(tree); */
+	/* printf("\n"); */
+	/* if (tree == NULL || func == NULL)*/
+	if (!tree || !func)
+		return;
+
+	/* printf(" function &print_num(%d)\n", tree->n); */
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
