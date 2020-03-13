bd8b9a1e0d67050122727690de6b2735ea2d73c6
juan-skill <1294@holbertonschool.com>
2020-03-13 19:04:05 +0000

    [fix] betty coding style 	modified:   3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,13 +6,13 @@
  * @tree: is a pointer to the root node of the tree to delete
  * Return: void
  */
- void binary_tree_delete(binary_tree_t *tree)
- {
-	 if (tree == NULL)
-		 return;
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
 
-	 binary_tree_delete(tree->left);
-	 binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
-	 free(tree);
- }
+	free(tree);
+}
